046ae14cdde45cbf9cdf787a6d1043b163b7ce9f
Alexander Perov <www.shurik_47@mail.ru>
2021-12-23 20:38:23 +0300

    The basics of redux

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css';
+import {useSelector, useDispatch} from "react-redux";
+
+function App() {
+  const dispatch = useDispatch()
+  const cash = useSelector( state => state.cash)
+  console.log(cash)
+
+  const addCash = (cash) => {
+    dispatch({type: "ADD_CASH", payload: cash})
+  }
+  const getCash = (cash) => {
+    dispatch({type: "GET_CASH", payload: cash})
+  }
+    return (
+        <div className={'app'}>
+          <div style={{fontSize: "3rem"}}>{cash}</div>
+            <div style={{display: "flex"}}>
+                <button onClick={() => addCash(Number(prompt()))}>Пополнить счет</button>
+                <button onClick={() => getCash(Number(prompt()))}>Снять со счета</button>
+            </div>
+        </div>
+    );
+}
+
+export default App;
